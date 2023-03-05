5861d5c8c9980ba435003819456018d7cccc52e5
trentb10 <trentbmin@gmail.com>
2023-03-04 20:37:01 -0500

    Added project files with basic functionality

---
+++ Messages/BuildEmbed.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Text;
+using DSharpPlus.Entities;
+
+public class Embed
+{
+  public DiscordEmbedBuilder BuildEmbedSimple(
+    DiscordEmbedBuilder embed
+  ){
+    embed.Build();
+
+    return embed;
+  }
+}
+
+public class GenericEmbed : Embed
+{
+  public DiscordEmbedBuilder SendGeneric(BotMessage msg)
+  {
+    DiscordEmbedBuilder embed = new DiscordEmbedBuilder
+    {
+      Description = msg.Description
+    };
+
+    return BuildEmbedSimple(embed);
+  }
+}
